e79ece10bbb3a9db7b34b7f323f91111f71a808d
yerbol777 <yerbol777@gmail.com>
2016-09-15 22:48:48 +0600

    + courts

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var instructors = require('./routes/instructors');
+var courts = require('./routes/courts');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/instructors', instructors);
+app.use('/courts', courts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
