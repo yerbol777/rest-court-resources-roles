026d9a87211fe20f697b46bf645f976f2086ddd1
aquio <alimzhan.zhanseitov@gmail.com>
2016-09-16 16:18:39 +0600

    added cross-origin accept permission

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 app.use('/instructors', instructors);
